fdb97007c1600094a5ad1916934aac4d537e9490
rares <purtan.rares@yahoo.com>
2020-12-03 21:46:53 +0000

    part 2 day 3 solved

--- src/Day3.java
+++ src/Day3.java
@@ -28,21 +28,26 @@ public class Day3
         }
     }
 
-    public void getTreeCount()
+    public int getTreeCount(int rowIncrement, int colIncrement)
     {
         int col=0;
         int treeCount=0;
-        for (int row = 0; row < elements.size(); row++)
+        for (int row = 0; row < elements.size(); row+=rowIncrement)
         {
             if (elements.get(row).charAt(col % elements.get(0).length()) == '#') treeCount++;
-            col += 3;
+            col += colIncrement;
         }
-        System.out.println(treeCount);
+        return treeCount;
     }
 
     public static void main(String[] args)
     {
         Day3 day3 = new Day3("day3input.txt");
-        day3.getTreeCount();
+        int test1 = day3.getTreeCount(1, 1);
+        int test2 = day3.getTreeCount(1, 3);
+        int test3 = day3.getTreeCount(1, 5);
+        int test4 = day3.getTreeCount(1, 7);
+        int test5 = day3.getTreeCount(2, 1);
+        System.out.println(test1 * test2 * test3 * test4 * test5);
     }
 }
