8739b022ceea61ad6a652036020a011ae83784c0
rares <purtan.rares@yahoo.com>
2020-12-12 17:09:00 +0000

    day6 finished

--- src/Day6.java
+++ src/Day6.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day6
 {
@@ -67,11 +66,83 @@ public class Day6
         System.out.println(totalcount);
     }
 
-    public static void main(String[] args)
+    ArrayList<String[]> questionsPart2 = new ArrayList<>();
+
+    public void part2(String filename)
     {
-        Day6 day6 = new Day6("day6input.txt");
-        day6.makeCharsUnique();
-        day6.printCount();
+        try
+        {
+            ArrayList<String> elements = new ArrayList<>();
+            File file = new File(filename);
+            Scanner scan = new Scanner(file);
+            StringBuilder group = new StringBuilder("");
+            String curLine;
+            while (scan.hasNext())
+            {
+                curLine = scan.nextLine();
+                if (!curLine.equals(""))
+                {
+                    group.append(curLine);
+                    group.append(" ");
+                }
+                else
+                {
+                    questionsPart2.add(group.toString().split(" "));
+                    group.delete(0, group.length());
+                }
+            }
+            questionsPart2.add(group.toString().split(" "));
+            group.delete(0, group.length());
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
     }
 
+    public void part2Answer()
+    {
+        Map<Character, Integer> charCount;
+        int totalAnswered = 0;
+        for (String[] curGroup : questionsPart2)
+        {
+            charCount = new HashMap<>();
+            int size = curGroup.length;
+
+            for (String person : curGroup)
+            {
+                for (int i = 0; i < person.length(); i++)
+                {
+                    if (!charCount.containsKey(person.charAt(i)))
+                    {
+                        charCount.put(person.charAt(i), 1);
+                    }
+                    else
+                    {
+                        int c = charCount.get(person.charAt(i));
+                        charCount.put(person.charAt(i), ++c);
+                    }
+                }
+            }
+            for (Character c : charCount.keySet())
+            {
+                if (charCount.get(c) == size)
+                {
+                    totalAnswered++;
+                }
+            }
+        }
+        System.out.println(totalAnswered);
+    }
+
+    public static void main(String[] args)
+    {
+        String filename = "day6input.txt";
+        Day6 day6 = new Day6(filename);
+        day6.makeCharsUnique();
+        day6.printCount();
+        day6.part2(filename);
+        day6.part2Answer();
+    }
 }
